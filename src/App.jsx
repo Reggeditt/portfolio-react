fcd98c38a8178c4ea83bcdf05d8b5123324badc7
Reggeditt <tkodjosarso@gmail.com>
2023-10-26 22:38:38 +0000

    update content

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Experience />
       <Tech />
       <Works />
-      <Feedbacks />
+      {/* <Feedbacks /> */}
       <div className="relative z-0">
         <Contact />  
       </div>       
